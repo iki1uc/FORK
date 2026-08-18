7c8726e4cd0e9bfefcdb972c4da4f75421b94cff
iki1uc <entertrainer@web.de>
2026-08-18 18:43:02 +0200

    Update nc.loader.js

--- nc.loader.js
+++ nc.loader.js
@@ -1,1 +1,10 @@
+import * as CORE from "./fork.core.js";
+import * as VECTOR from "./fork.vector.js";
+import * as OPS from "./fork.ops.js";
+
+export const NC = {
+    CORE,
+    VECTOR,
+    OPS
+};
 
